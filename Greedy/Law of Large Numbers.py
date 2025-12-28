b4c42255a24d57097cf0aa75d24ac536ddc94c67
Donghyeok Kang <rkdehdgur432@naver.com>
2025-12-28 17:58:32 +0900

    Update Law of Large Numbers.py
    map/언패킹 입력 처리 문법을 배웠지만 실전에서 바로 떠오르지 않았다.
    큰 수를 K번 더하고 두 번째 수를 1번 더하는 패턴을 반복문으로 구현했는데,
    이 문제는 반복 횟수를 수식으로 먼저 계산해 반복을 줄일 수 있다는 걸 알게 됐다.
    다음부터는 구현 전에 패턴을 식으로 만들 수 있는지 먼저 생각해보자.

--- Greedy/Law of Large Numbers.py
+++ Greedy/Law of Large Numbers.py
@@ -52,4 +52,20 @@ for i in range(int(inputInfo[1])):
     count+=1
 print(sum)
 
+### 책 답 ###
 
+n,m,k = map(int, input().split(' '))
+data = list(map(int, input().split(' ')))
+
+data.sort()
+first=data[n-1]
+second=data[n-2]
+
+count = int(m/(k+1))*k
+count+= m%(k+1)
+
+result = 0
+result+=(count)*first
+result +=(m-count) *second
+
+print(result)
