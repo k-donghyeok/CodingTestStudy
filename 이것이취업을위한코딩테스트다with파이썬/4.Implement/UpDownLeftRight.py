fdd24bd2ee6e687a483a3769f3262426fa9c812d
k-donghyeok <rkdehdgur432@naver.com>
2026-04-19 13:25:03 +0900

    Update UpDownLeftRight.py
    나중에 여러가지 탐색을 위해서 dx dy 방식이 필수라고해 이방식으로 익숙해지는중

--- 이것이취업을위한코딩테스트다with파이썬/4.Implement/UpDownLeftRight.py
+++ 이것이취업을위한코딩테스트다with파이썬/4.Implement/UpDownLeftRight.py
@@ -1,10 +1,10 @@
 size=int(input())
-move=input().split()
+orders=input().split()
 
 coordinate=[1,1]
 
-
-for count in range(len(move)):
+# 처음 풀었을때
+""" for count in range(len(move)):
     if(move[count]=='U'):
         coordinate[0]+=-1
         if(coordinate[0]<1):
@@ -24,5 +24,23 @@ for count in range(len(move)):
         coordinate[1]+=1
         if(coordinate[1]>size):
             coordinate[1]+=-1
-        continue
-print(coordinate)
+        continue """
+
+
+# dx dy 방식으로 풀어봤을때
+x=1
+y=1
+move =['U','D','L','R']
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+for order in orders:
+    for count in range(len(move)):
+        if(move[count]==order):
+            nx=x+dx[count]
+            ny=y+dy[count]
+            if(nx<1 or ny<1 or nx>size or ny>size):
+                continue
+            x=nx
+            y=ny
+
+print(f"{x},{y}")
