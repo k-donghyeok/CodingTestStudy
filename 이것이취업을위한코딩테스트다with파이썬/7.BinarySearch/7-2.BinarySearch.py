e8908f44794bb3cbf3464b78fec9f08817bae5fb
k-donghyeok <rkdehdgur432@naver.com>
2026-04-25 14:10:49 +0900

    Update 7-2.BinarySearch.py
    중간값을 시작점과 종료점을 더한값으로 나누기2한 몫
    
    중간값을 제외하면서 범위를 줄여나간다
    
    목표가 중간값보다 크면 시작값으로 중간값+1
    
    작으면 종료값을 중간값-1
    
    시작값 과 종료값의 크기가 바뀌면 찾고자 하는 값이 없는경우

--- 이것이취업을위한코딩테스트다with파이썬/7.BinarySearch/7-2.BinarySearch.py
+++ 이것이취업을위한코딩테스트다with파이썬/7.BinarySearch/7-2.BinarySearch.py
@@ -2,17 +2,22 @@ array=[0,2,4,6,8,10,12,14,16,18]
 count=0
 
 def binarySearch(array,start,end,target):
+    if(start>end):
+        return -1
     
     
-    
-    middle=(end//2)+start
+    middle=(end+start)//2
     if(array[middle]>target):
-        return binarySearch(array,0,middle,target)
+        return binarySearch(array,start,middle-1,target)
     elif(array[middle]<target):
-        return binarySearch(array,middle,end,target)
+        return binarySearch(array,middle+1,end,target)
     else:
         return middle
 
 target=int(input('찾고자 하는 숫자를 입력하세요:'))
 array=list(map(int,input('배열을 입력하세요:').split()))
-print(f"{target}은 배열의 {binarySearch(array,0,len(array)-1,target)}번째 인덱스에 있습니다") 
+result=binarySearch(array,0,len(array)-1,target)
+if(result==-1):
+    print(f"{target}은 배열에 존재하지 않습니다") 
+else:
+    print(f"{target}은 배열의 {result}번째 인덱스에 있습니다") 
