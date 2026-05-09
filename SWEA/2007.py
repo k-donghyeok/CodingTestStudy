1ac3a9e266367829f3c73feb45d631cede737f1d
k-donghyeok <rkdehdgur432@naver.com>
2026-05-09 18:06:42 +0900

    Create 2007.py

---
+++ SWEA/2007.py
@@ -0,0 +1,20 @@
+# 배열을 1칸 2칸 3칸 씩 크기를 늘려가면서
+# 다음거랑 그전거랑 같은지 비교하다가 같은 크기일때찾기
+
+#5 46
+
+T = int(input())
+
+for test_case in range(1,1+T):
+    strinput=input()
+
+    go=True
+
+    while(go):
+
+        for length in range(1,len(strinput)):
+
+            if(strinput[:length]==strinput[length:length*2]):
+                print(f'#{test_case} {length}')
+                go=False
+                break
