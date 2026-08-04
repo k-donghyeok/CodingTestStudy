46860b9ce8c6e9eeeb2bb2ac10db5800bf271365
k-donghyeok <rkdehdgur432@naver.com>
2026-08-04 16:45:48 +0900

    1215 파일 생성

---
+++ SWEA/java/d3/1215.java
@@ -0,0 +1,11 @@
+//15:33
+
+//2차원 배열을 완점탐색으로 입력받은 크기만큼 
+// 전체크기 - 입력크기 인덱스 까지 탐색해야함
+// 가로는 그냥 탐색하면 되는데 
+// 세로는 반복문을 도는데 돌면서 가져오는 글자를 하나씩 스트링에 더해줘야함
+
+BufferReader br = new BufferReader(new InputStreamReader(System.in));
+
+
+int T=Integer.parseInt(br.readLine()); 
